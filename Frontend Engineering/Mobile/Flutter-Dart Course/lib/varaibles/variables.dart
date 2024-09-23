009b85d6c803e335b998d2f9df4b03db1b0a2fd9
unawarexi <unawarexi@gmail.com>
2024-09-23 23:47:56 +0100

    updated to start learning flutter

---
+++ Frontend Engineering/Mobile/Flutter-Dart Course/lib/varaibles/variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String productName = "mobile phone";
+  int price = 750000;
+  int quantity = 4;
+
+  int totalPrice = price * quantity;
+  int stockAvailable = 10;
+
+  print("Product: $productName");
+  print("Price: $price");
+  print("Quantity: $quantity");
+  print("totalproice: $totalPrice");
+}
