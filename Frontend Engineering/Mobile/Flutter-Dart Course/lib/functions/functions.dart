94f8367c4da84f777f56223b253eabcfa5184060
unawarexi <unawarexi@gmail.com>
2024-09-24 11:39:48 +0100

    Updated variables, functions, datatypes and conditionals

---
+++ Frontend Engineering/Mobile/Flutter-Dart Course/lib/functions/functions.dart
@@ -0,0 +1,15 @@
+///  they're different ways of declaring a function in dart
+///
+///
+library;
+
+/// this function void tells dart it should not expect to return any values
+void functionName() {}
+
+int multiplyNumbers() {}
+
+String addWords() {
+  String firstWord = 'president';
+  String secondWord = "confederate";
+  return ("$firstWord $secondWord");
+}
