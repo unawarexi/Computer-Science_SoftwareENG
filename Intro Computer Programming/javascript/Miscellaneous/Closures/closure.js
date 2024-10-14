797acaf0dba36c50868caf2f5f6b5be711030dd7
unawarexi <unawarexi@gmail.com>
2024-10-14 16:53:52 +0100

    formatted

--- Intro Computer Programming/javascript/Miscellaneous/Closures/closure.js
+++ Intro Computer Programming/javascript/Miscellaneous/Closures/closure.js
@@ -3,13 +3,13 @@
  *  it is known as nested function
  *  the inner function should be called inside the outer function body
  *  the inner function can access values in the outer but outer can't
- * 
+ *
  *  it depends on lexical envirenments
  *   1 environment record == the main object it's in
  *   2 reference to outer lexical environ
- * 
+ *
  *  ================== what are closures? =================
- * they're inner functions 
+ * they're inner functions
  *  they're bundled together with the lexical environ : datatypes
  *  they're declared inside the datatype
  *    in other words the inner functions contains the var of the outer function
@@ -17,15 +17,14 @@
 
 const crust = "crust";
 function earth() {
-    let center = "mantle"
+  let center = "mantle";
 
-    function message(){
-        let deep = "core"
+  function message() {
+    let deep = "core";
 
-        console.log(`the earth is made up of the ${crust} ${center} and ${deep}`)
-    }
+    console.log(`the earth is made up of the ${crust} ${center} and ${deep}`);
+  }
 
-    message();
-
+  message();
 }
 earth();
--- Intro Computer Programming/javascript/Syntax & Structure/Functions/recursion.js
+++ Intro Computer Programming/javascript/Syntax & Structure/Functions/recursion.js
@@ -2,37 +2,32 @@
  * they're functions that calls itself inisde a function body
  * it stops itself
  * they must be a condition to stop the function else inifinite loop
- * 
+ *
  * this condition is called the "base case"
  */
 
+const count = (num) => {
+  // prints in reverse
+  console.log(num);
 
-const count = (num) => { // prints in reverse
-    console.log(num)
+  let updated = num - 1;
 
-    let updated = num - 1
+  if (updated >= 0) {
+    //base case
+    count(updated); // recursive
+  }
+};
 
-    if (updated >= 0 ){ //base case
-        count(updated) // recursive
-    }
-
-}
-
-count(10)
-
-
+count(10);
 
 const numbers = (num1) => {
-    console.log(num1)
+  console.log(num1);
 
-    let updatedNum = num1 + 1
+  let updatedNum = num1 + 1;
 
-    if (updatedNum <= 10 ){
-        numbers(updatedNum)
-    }
+  if (updatedNum <= 10) {
+    numbers(updatedNum);
+  }
+};
 
-}
-
-numbers(0)
-
-
+numbers(0);
