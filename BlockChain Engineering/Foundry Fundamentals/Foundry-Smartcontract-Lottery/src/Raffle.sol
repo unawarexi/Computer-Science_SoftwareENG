967f68f5b8ba983fbfb9ead7efe8eaab879cfb69
unawarexi <unawarexi@gmail.com>
2025-07-07 02:43:58 +0100

    learning events and emitters , blockt timestamp

--- BlockChain Engineering/Foundry Fundamentals/Foundry-Smartcontract-Lottery/src/Raffle.sol
+++ BlockChain Engineering/Foundry Fundamentals/Foundry-Smartcontract-Lottery/src/Raffle.sol
@@ -2,27 +2,44 @@
 pragma solidity ^0.8.19;
 
 contract Raffle {
-    address[] private players;
+
+    error Raffle_NotEnoughETH(uint256 sent, uint256 required);
+
+
+    address payable[] private s_players;
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
-    error NotEnoughETH(uint256 sent, uint256 required);
+    event Raffle_Entered(address indexed player, uint256 amount);
 
-    constructor(uint256 entranceFee) {
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp; // Initialize the last timestamp to the current block timestamp
     }
-
+    // Function to enter the raffle
+    // The function requires a minimum amount of ETH to be sent with the transaction
     function enterRaffle() public payable {
         // require(
         //     msg.value >= i_entranceFee,
         //     "Not enough ETH to enter the raffle"
         // );
         if (msg.value < i_entranceFee) {
-            revert NotEnoughETH(msg.value, i_entranceFee);
+            revert Raffle_NotEnoughETH(msg.value, i_entranceFee);
         }
-        players.push(msg.sender);
+        s_players.push(payable(msg.sender));
+        emit Raffle_Entered(msg.sender, msg.value);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external view {
+        // Check if enough time has passed since the last winner was picked
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert("Not enough time has passed since the last winner was picked");
+        }
+        
+    }
 
     // View functions to retrieve information about the raffle
     function getEntranceFee() external view returns (uint256) {
