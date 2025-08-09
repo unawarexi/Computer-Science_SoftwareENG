fca4e9ee60b3639efc728069522d3d1ad4ec5580
unawarexi <unawarexi@gmail.com>
2025-08-09 15:41:13 +0100

    Interface for rebase token

---
+++ BlockChain Engineering/Advanced Foundry/Foundry-Ccip-Rebase-Token/src/interfaces/IRebaseToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+interface IRebaseToken {
+    // Interface for the Rebase Token contract
+
+
+    function mint(address _to, uint256 _amount) external;
+
+
+    function burn(address _from, uint256 _amount) external;
+}
