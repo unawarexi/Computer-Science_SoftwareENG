42435ab0ac74d1adce4a7e17c599eb0622d81f3b
unawarexi <unawarexi@gmail.com>
2026-08-13 05:51:14 +0100

    updated

--- Intro-Computer-Programming/07-Go/12-Projects/go.mod
+++ Intro-Computer-Programming/07-Go/12-Projects/go.mod
@@ -2,4 +2,4 @@ module github.com/unawarexi/12-projects
 
 go 1.26.5
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- Intro-Computer-Programming/07-Go/12-Projects/main.go
+++ Intro-Computer-Programming/07-Go/12-Projects/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 
 func main(){
 	fmt.Println("Hello World")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	PortString := os.Getenv("PORT")
 	if PortString == "" {
